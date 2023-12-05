88c22d30d2f74b0c7070325aec9f21841944e606
Guy Richie <qwabs94@gmail.com>
2023-12-05 16:52:13 +0000

    Initiall Commit for email automation

---
+++ emailAutomation.js
@@ -0,0 +1,37 @@
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
+
+// Configure your Gmail account
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'richiebgitcall94@gmail.com',
+    pass: '@2xbet,com_94!',
+  },
+});
+
+// Define the email options
+const mailOptions = {
+  from: 'richiebgitcall94@gmail.com',
+  to: 'qwabs94@gmail.com',
+  subject: 'Scheduled Email',
+  text: 'This is an automated email sent using Node.js and Gmail.',
+};
+
+// Schedule the email to be sent every day at 9 AM
+cron.schedule('*/1 * * * *', () => {
+  sendEmail();
+})
+
+// { timezone: 'Western Sahara Standard Time' }); // Set your timezone (e.g., 'America/New_York')
+
+// Function to send the email
+function sendEmail() {
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+}
