e0d2d76291dd3e4e8aef98de28475a72e7fd5411
Guy Richie <qwabs94@gmail.com>
2023-12-06 13:42:40 +0000

    Make changes to the .env file

--- emailAutomation.js
+++ emailAutomation.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   secure: false,
   auth: {
     user: 'richiebgitcall94@gmail.com',
-    pass: '@2xbet,com_94!',
+    pass: process.env.PASSWORD,
   },
 });
 
